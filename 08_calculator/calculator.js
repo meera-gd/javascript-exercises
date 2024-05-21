1851856b7f1bcb914a7cfe452c7cc6d7a7cb0968
Meera Godden <meera.s.gd@gmail.com>
2024-05-20 22:03:28 -0700

    Solved 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, item) => total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, item) => product * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
