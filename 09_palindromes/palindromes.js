d880568a989877a516673569d47034a0a12777e4
Meera Godden <meera.s.gd@gmail.com>
2024-05-20 22:11:50 -0700

    Solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	const stripped = string.toLowerCase().replace(/[ \t\n,.!?]/g, '');
+	return stripped.split('').reverse().join('') === stripped;
 };
 
 // Do not edit below this line
